f64a192bc7f40c26fc46e85e3b852e1c49ffdbfe
Kaleb <kaleb.stalnacke@gmail.com>
2023-08-29 19:49:02 +0200

    Included equation for calculating grind setting

--- Coffee.cs
+++ Coffee.cs
@@ -4,34 +4,26 @@ namespace KStalnacke.Coffee
 {
     class Program
     {
+        // Determines the amount of coffee beans needed based on the number of cups.
         private static int CalculateGrams(int numberOfCups)
         {
-            // Determines the amount of coffee beans needed based on the number of cups.
             int amountOfCoffee = numberOfCups * 125 / 15;
             return amountOfCoffee;
         }
 
-        private static int CalculateGrind(int grams)
+        // Uses a function based on data from r/JamesHoffman to calculate the Wilfa Uniform grind setting.
+        private static double CalculateGrind(int grams)
         {
-            int[] gramThresholds = { 25, 50, 60 };
-            int[] grindSizes = { 18, 22, 26 };
-
-            for (int i = 0; i < gramThresholds.Length; i++)
-            {
-                if (grams <= gramThresholds[i])
-                {
-                    return grindSizes[i];
-                }
-            }
-
-            // Handle the case when the grams are larger than all thresholds
-            return grindSizes[gramThresholds.Length - 1];
+            double grindSize = Math.Floor(0.005 * Math.Pow(grams, 2) - 0.16 * grams + 21.24);
+            return grindSize;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the amount of cups you want to use:");
             int numberOfCups = Convert.ToInt16(Console.ReadLine());
+            int gramsNeeded = CalculateGrams(numberOfCups);
+            Console.WriteLine($"For {numberOfCups} cups of coffee, you will need {gramsNeeded} grams of coffee beans. The beans should be grinded using setting {CalculateGrind(gramsNeeded)}.");
             Console.ReadKey();
         }
     }
